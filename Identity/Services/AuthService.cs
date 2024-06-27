ae68be7baa5182273a2af35f651617927989ae83
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-06-27 23:19:52 +0545

    Complete User Login and Registration Service with JWT

---
+++ Identity/Services/AuthService.cs
@@ -0,0 +1,111 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Application.Contract.Identity;
+using Application.Exceptions;
+using Application.Models.Identity;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Identity.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly JwtSettings _jwtSetting;
+
+        public AuthService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IOptions<JwtSettings> jwtSetting)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _jwtSetting = jwtSetting.Value; 
+        }
+        public async Task<AuthResponse> Login(AuthRequest request)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null)
+            {
+                throw new NotFoundException($"User with {request.Email} not found.", request.Email);
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+            if (result.Succeeded == false)
+            {
+                throw new BadRequestException($"Credential for '{request.Email} aren't valid'");
+            }
+
+            JwtSecurityToken jwtSecurityToken = await GenerateToken(user);
+
+            var response = new AuthResponse
+            {
+                Id = user.Id,
+                Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+                Email = user.Email!,
+                UserName = user.UserName!
+            };
+
+            return response;
+        }
+
+        public async Task<RegistrationResponse> Register(RegistrationRequest request)
+        {
+            var user = new ApplicationUser
+            {
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                UserName = request.UserName,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Employee");
+                return new RegistrationResponse() { UserId = user.Id };
+            }
+            else
+            {
+                StringBuilder str = new StringBuilder();
+                foreach (var error in result.Errors)
+                {
+                    str.AppendFormat("•{0}\n", error.Description);
+                }
+
+                throw new BadRequestException($"{str}");
+            }
+        }
+
+        private async Task<JwtSecurityToken> GenerateToken(ApplicationUser user)
+        {
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var role = await _userManager.GetRolesAsync(user);
+
+            var roleClaims = role.Select(q => new Claim(ClaimTypes.Role, q)).ToList();
+
+            var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName!),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Email, user.Email!),
+                    new Claim("uid", user.Id)
+                }
+                .Union(userClaims)
+                .Union(roleClaims);
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSetting.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer:_jwtSetting.Issuer,
+                audience:_jwtSetting.Audience,
+                claims:claims,
+                expires:DateTime.Now.AddMinutes(_jwtSetting.DurationInMinute),
+                signingCredentials:signingCredentials
+                );
+            return jwtSecurityToken;
+        }
+    }
+}
---
+++ Identity/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using Application.Contract.Identity;
+using Application.Models.Identity;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<List<Employee>> GetEmployees()
+        {
+            var employees = await _userManager.GetUsersInRoleAsync("Employee");
+            return employees.Select(q => new Employee
+                {
+                    Id = q.Id,
+                    FirstName = q.FirstName,
+                    LastName = q.LastName,
+                    Email = q.Email
+                }).ToList();
+        }
+
+        public async Task<Employee> GetEmployee(string userId)
+        {
+            var employees = await _userManager.FindByIdAsync(userId: userId);
+            return new Employee
+            {
+                Email = employees.Email,
+                Id = employees.Id,
+                FirstName = employees.FirstName,
+                LastName = employees.LastName,
+            };
+
+        }
+    }
+}
